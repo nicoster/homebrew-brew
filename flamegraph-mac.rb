54e7b6b4fd10ded5c656548fa456cc24c226495b
Nick Xiao <nick.xiao@zoom.us>
2020-02-24 16:33:09 +0800

    fix build errors

--- flamegraph-mac.rb
+++ flamegraph-mac.rb
@@ -1,9 +1,9 @@
 class FlamegraphMac < Formula
     desc "Take stackshots with undocumented syscall to make flamegraph on MacOS"
     homepage "https://github.com/TheHipbot/weather"
-    url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/flamegraph-mac-master-4ce8175-20200224-145305.tar.gz"
-    sha256 "84dc0b30698a99af2f0b68528c8f8dc27c208ba944f60265f4fe531067a9ebaa"
-    version "1.2.0"
+    url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/flamegraph-mac-master-1ae4407-20200224-162909.tar.gz"
+    sha256 "100da657df51ae1ac0d6974e649cba8ceb677bb1b41cc946d4ff4fdee4cfe22d"
+    version "1.3.0"
   
     depends_on "luajit"
   
@@ -11,6 +11,10 @@ class FlamegraphMac < Formula
   
     def install
       bin.install "bin/mkflamegraph"
-      lib.install "lib/*"
+      lib.install "lib/flamegraph.pl"
+      lib.install "lib/foldstackshots.sh"
+      lib.install "lib/kcdata.py"
+      lib.install "lib/stackcollapse-stackshot.lua"
+      lib.install "lib/stackshots.lua"
     end
   end

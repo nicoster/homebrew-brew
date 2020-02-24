64436501ac01bfd0dd8a4d4a32247e983ef63dc2
Nick Xiao <nick.xiao@zoom.us>
2020-02-24 15:17:14 +0800

    bump verion

--- flamegraph-mac.rb
+++ flamegraph-mac.rb
@@ -3,7 +3,7 @@ class FlamegraphMac < Formula
     homepage "https://github.com/TheHipbot/weather"
     url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/flamegraph-mac-master-4ce8175-20200224-145305.tar.gz"
     sha256 "84dc0b30698a99af2f0b68528c8f8dc27c208ba944f60265f4fe531067a9ebaa"
-    version "1.1.0"
+    version "1.2.0"
   
     depends_on "luajit"
   

29dca429d2418ad582810886c2279cb6bd7d7f51
Nick Xiao <nick.xiao@zoom.us>
2020-02-24 15:14:14 +0800

    minor changes

--- flamegraph-mac.rb
+++ flamegraph-mac.rb
@@ -1,7 +1,7 @@
 class FlamegraphMac < Formula
     desc "Take stackshots with undocumented syscall to make flamegraph on MacOS"
     homepage "https://github.com/TheHipbot/weather"
-    url "https://github.com/nicoster/flamegraph-mac/blob/master/archive/flamegraph-mac-master-4ce8175-20200224-145305.tar.gz"
+    url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/flamegraph-mac-master-4ce8175-20200224-145305.tar.gz"
     sha256 "84dc0b30698a99af2f0b68528c8f8dc27c208ba944f60265f4fe531067a9ebaa"
     version "1.1.0"
   

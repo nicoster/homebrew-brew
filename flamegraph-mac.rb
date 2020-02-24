8261b34d1ca0dcd5fa12f08f34abc960bfcf3673
Nick Xiao <nick.xiao@zoom.us>
2020-02-24 16:46:00 +0800

    minor changes

--- flamegraph-mac.rb
+++ flamegraph-mac.rb
@@ -1,5 +1,5 @@
 class FlamegraphMac < Formula
-    desc "Take stackshots with undocumented syscall to make flamegraph on MacOS"
+    desc "Take stackshots with undocumented syscall to make flamegraph on MacOS. Check https://github.com/nicoster/flamegraph-mac for more details."
     homepage "https://github.com/TheHipbot/weather"
     url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/flamegraph-mac-master-1ae4407-20200224-162909.tar.gz"
     sha256 "100da657df51ae1ac0d6974e649cba8ceb677bb1b41cc946d4ff4fdee4cfe22d"
@@ -8,7 +8,13 @@ class FlamegraphMac < Formula
     depends_on "luajit"
   
     bottle :unneeded
-  
+ 
+    def caveats; <<~EOS
+        The tool is installed by the name of `mkflamegraph`
+
+    EOS
+    end
+
     def install
       bin.install "bin/mkflamegraph"
       lib.install "lib/flamegraph.pl"

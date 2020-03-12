81998ec5c7350041fa1d8d66258eeee6dca1f7fc
Nick Xiao <nick.xiao@zoom.us>
2020-03-12 13:10:41 +0800

    update artifacts

--- flamegraph-mac.rb
+++ flamegraph-mac.rb
@@ -3,24 +3,24 @@ class FlamegraphMac < Formula
     homepage "https://github.com/nicoster/flamegraph-mac"
     url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/flamegraph-mac-master-592dfc4-20200312-105850.tar.gz"
     sha256 "0d752b0c5cf449b234c7cdde902c93dd847b3d3ecbc20bcbbe96f56ec83ef23a"
-    version "1.3.6"
+    version "1.3.7"
   
     depends_on "luajit"
   
     bottle :unneeded
  
     def caveats; <<~EOS
-        This tool is installed by the name of `mkflamegraph` to:
-            /usr/local/bin/mkflamegraph
+        This tool is installed by the name of `mkflame` to:
+            /usr/local/bin/mkflame
 
         It requires root privileges to take stackshots of another process. 
-        mkflamegraph invokes `sudo` internally when taking stackshots.  
+        mkflame invokes `sudo` internally when taking stackshots.  
 
     EOS
     end
 
     def install
-      bin.install "bin/mkflamegraph"
+      bin.install "bin/mkflame"
       lib.install "lib/flamegraph.pl"
       lib.install "lib/foldstackshots.sh"
       lib.install "lib/kcdata.py"

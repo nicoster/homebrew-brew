ed276600cc5d50b3eae30caf42f8c27fe9ca742f
Nick Xiao <nick.xiao@zoom.us>
2020-03-12 13:14:22 +0800

    rename to mkflame

--- update-rb.sh
+++ update-rb.sh
@@ -2,4 +2,4 @@
 
 build=`ls -1 ../archive/`
 echo $build
-sed -i -e 's!url ".*"!url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/'$build'"!; s!sha256.*!sha256 "'$(sha256sum ../archive/$build|awk '{print $1}')'"!' flamegraph-mac.rb
+sed -i -e 's!url ".*"!url "https://github.com/nicoster/flamegraph-mac/raw/master/archive/'$build'"!; s!sha256.*!sha256 "'$(sha256sum ../archive/$build|awk '{print $1}')'"!' mkflame.rb
